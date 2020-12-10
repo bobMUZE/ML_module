d4e9b8139d48092b4d1416547e07f415d58f05cb
sky81219 <52487610+sky81219@users.noreply.github.com>
2020-12-10 14:28:38 +0900

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -305,13 +305,14 @@ class Preprocessing:
             "DomainRegistrationLength": [self.DomainRegistrationLength()], "HavingIp": [self.HavingIp()],
             "sfh": [self.sfh()], "short_url_service": [self.shortening_service()], "favicon": [self.Favicon()]}
         data = pd.DataFrame(data_)
-        print(data)
 
         return data
 
 class ML:
-    def __init__(self, json_path, csv_path):
-        self.json_data = pd.read_json(os.path.abspath(json_path))
+    def __init__(self, csv_path, time, url_file, xpath):
+        self.time = time
+        self.url_file = url_file
+        self.xpath = xpath
         self.csv_data = pd.DataFrame(csv_path)
         self.x = self.csv_data[["entropy", "pathentropy", "hostname_length", "path_length", "tld_length",
                                 "count-", "count-@", "special_chacter", "count-http", "count-https", "count-www",
@@ -322,21 +323,18 @@ class ML:
     def Predict_Proba(self):
         prediction = joblib.load("forest_model_request.pkl")
         model_finally = prediction.predict_proba(self.x)
-        print(model_finally)
         return model_finally
 
     def DecisionPrediction(self):
         prediction = joblib.load("forest_model_request.pkl")
         binary_prediction = prediction.predict(self.x)
-        print(binary_prediction)
         return binary_prediction
 
     def PredictionData(self):
-        json_data = []
-        for i in self.json_data["data"]:
-            json_data.append(i["url"])
         ex_web = [site for site in self.csv_data["url"]]
-
+        url_state = requests.get(self.url_file).status_code # 2020-12-10 추가
+        req_time = requests.get(self.url_file).elapsed # 2020-12-10 추가
+        
         predict_list = []
         for value in self.Predict_Proba():
             if value[0] > value[1]:
@@ -345,45 +343,29 @@ class ML:
                 predict_list.append("{}%".format(int(value[1] * 100)))
 
         making_log_data = OrderedDict()
-        log_path = "log.json"
-        f = open(log_path, "r", encoding="utf-8")
-        dict_info = json.loads(f.read())
-        making_log_data["Timestamp"] = f"{datetime.datetime.now()}"
+        making_log_data["Timestamp"] = f"{self.time}"
         making_log_data["detection"] = True
-        making_log_data["URL"] = f"url sample"
-        making_log_data["xpath"] = "xpath"
+        making_log_data["URL"] = f"{self.url_file}"
+        making_log_data["response"] = f"{str(url_state)}", # 2020-12-10 추가 
+        making_log_data["request_time"] = f"{str(req_time)}", # 2020-12-10 추가
+        making_log_data["xpath"] = f"{self.xpath}"
 
         making_log_data["module"] = "ML_PhishingDetected"
         making_log_data["log"] = []
 
-        for i in range(0, len(json_data)):
+        for i in range(0, len(ex_web)):
             # 실제 하나 당 로그는 이 아래에서
             logdata = {"submodule": 0,
-                       "internal_url": f"{json_data[i]}",
                        "external_url": f"{ex_web[i]}",
                        "result": f'{self.DecisionPrediction()[i]}',
                        "percentage": f"{predict_list[i]}"
                        }
             making_log_data["log"].append(logdata)
-
-            print(json.dumps(making_log_data, ensure_ascii=False, indent="\t"))
-
-            f.close()
-            f = open(log_path, "w", encoding="utf8")
-            dict_info.append(making_log_data)
-            f.write(json.dumps(dict_info, ensure_ascii=False, indent='\t'))
-
-
-if __name__ == "__main__":
-    data_stack = []
-    path = os.path.abspath("Ex.json")
-    data = pd.read_json(path)
-
-    for i in data["data"]:
-        data_stack.append(Preprocessing(i["ex_url"]).MakingData())
-    data_add = pd.concat(data_stack)
-    making_data = pd.DataFrame(data_add)
-    making_data.to_csv("malicious.csv", index=False)
-    ML(path, making_data).PredictionData()
-    ML(path, making_data).Predict_Proba()
+        testcol.insert_one(making_log_data) # 몽고 DB 추가 데이터 넣는곳
+ 
+# 몽고DB 
+class MongoDbManager:
+    def __init__(self):
+        self._instance = None
+        self.client = pymongo.MongoClient("mongodb://muze_root:this-is-root-passwd@3.13.31.198:27017/")
 
